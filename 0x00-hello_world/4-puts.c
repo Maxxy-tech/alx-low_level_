7454bdf40404530e2eff4a9f43b825abe16f702d
maxxy-tech <maxxytech99@gmail.com>
2022-12-09 03:57:24 -0800

    compiled

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,10 @@
-#include <stdio.h>                                                                                                                      
+#include <stdio.h>
 
-/**                                                                                                                                     
+int main(void)
+{
+	 puts("Programming is like building a multi lingual puzzle\n" );                                                            
 
- * main - Entry                                                                                                                         
-
- * Return: Always 0                                                                                                                     
-
- */                                                                                                                                     
-
-int main(void)                                                                                                                          
-
-{                                                                                                                                       
-
-        puts("\"Programming is like building a multi lingual puzzle);                                                            
-
-        return (0);
+		         return (0);
 
 }
 
