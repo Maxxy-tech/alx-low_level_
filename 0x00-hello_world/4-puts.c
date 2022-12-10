8b11d14244f83bfe06913d32759034c65f89a28c
maxxy-tech <maxxytech99@gmail.com>
2022-12-10 11:49:29 -0800

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>             
+
 /**
  *  main - prints out a string via puts
  *  Description: program that prints out the string followed by a new line
  *  Return: int 0 when done and no errors        
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
