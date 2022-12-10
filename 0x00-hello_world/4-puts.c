6b610b519eb03b4fdc0cd99dd63a42c4a894cef5
Maxxy-tech <117773419+Maxxy-tech@users.noreply.github.com>
2022-12-10 21:00:56 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>             
-
 /**
  *  main - prints out a string via puts
  *  Description: program that prints out the string followed by a new line
  *  Return: int 0 when done and no errors        
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
